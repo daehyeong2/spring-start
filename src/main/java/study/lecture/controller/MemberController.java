b1ca14af5354b63c98d5e3ad774f61dea60105d3
daehyeong2 <baconbacon1231@gmail.com>
2025-05-02 16:06:15 +0900

    Section 6: 회원 웹 기능 - 조회

--- src/main/java/study/lecture/controller/MemberController.java
+++ src/main/java/study/lecture/controller/MemberController.java
@@ -1,7 +1,9 @@
 package study.lecture.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import study.lecture.domain.Member;
@@ -30,5 +32,12 @@ public class MemberController {
     memberService.join(member);
 
     return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model){
+    List<Member> result = memberService.findMembers();
+    model.addAttribute("members", result);
+    return "members/memberList";
   }
 }
