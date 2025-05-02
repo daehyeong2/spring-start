20ab3f10bd3f279e0e4d25bde3000d9fa2699730
daehyeong2 <baconbacon1231@gmail.com>
2025-05-02 15:54:48 +0900

    Section 6: 회원 웹 기능 - 등록

--- src/main/java/study/lecture/controller/MemberController.java
+++ src/main/java/study/lecture/controller/MemberController.java
@@ -2,6 +2,9 @@ package study.lecture.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.lecture.domain.Member;
 import study.lecture.service.MemberService;
 
 @Controller
@@ -11,5 +14,21 @@ public class MemberController {
   @Autowired
   public MemberController(MemberService memberService){
     this.memberService = memberService;
+  }
+
+  @GetMapping("/signup")
+  public String signupForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/signup")
+  public String create(MemberForm form){
+    Member member = new Member();
+    member.setName(form.getUsername());
+
+    System.out.println("member = " + member.getName());
+    memberService.join(member);
+
+    return "redirect:/";
   }
 }
